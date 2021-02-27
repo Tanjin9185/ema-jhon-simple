49055ca776f4acfff1978196e10b2ba89fd40685
Tanjin <tanjin.ahamed123@gmail.com>
2021-02-27 17:54:38 +0600

    all most done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+//rsc
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="siteLogo">
+                <img src={logo} alt=""/>
+            </div>
+            <div className="nav-item">
+                <nav>
+                    <a href="/shop" >Shop</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/manage">Manage Inventory</a>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
